2c094a5783f2ade807eff9c1adf4500b50477a6b
aricalves <aric.alves2012@gmail.com>
2017-10-25 17:22:20 -0700

    Add some working code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,44 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      video: window.exampleVideoData[0],
+      videos: window.exampleVideoData,
+      select: false
+    };
+  }
+  
+  onVideoClick() {
+    console.log('Video clicked');
+    this.setState({
+      select: !this.state.select
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.video} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videos} onClick={this.onVideoClick.bind(this)}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+
+
+
